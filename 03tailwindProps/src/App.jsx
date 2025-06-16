d7efc0e2322a301e37846b765dd7d4fb75db69df
MayurChaudhary198 <chaudharymayur198@gmail.com>
2025-06-16 17:27:19 +0530

    tailwind prac

---
+++ 03tailwindProps/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+   <h1 className="text-4xl font-semibold text-blue-500 mb-4 ">React Props</h1>
+
+   <div className="max-w-xs p-6 rounded-md shadow-md bg-black">
+        <img
+          src="https://images.pexels.com/photos/8597551/pexels-photo-8597551.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+          alt=""
+          className="object-cover object-center w-full rounded-md h-72 bg-gray-500"
+        />
+        <div className="mt-6 mb-2">
+          <span className="block text-sm font-medium font-mono tracking-widest uppercase text-indigo-400">
+            Title
+          </span>
+          <h2 className="text-xl font-semibold tracking-wide">Lorem ipsum dolor</h2>
+        </div>
+        <p className="text-gray-300">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio tempora ipsum soluta
+          amet
+        </p>
+      </div>
+   </>
+  )
+}
+
+export default App
